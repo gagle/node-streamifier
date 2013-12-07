cc864ff561f329ffa0123135bf426b328abfca1a
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2013-12-07 14:47:02 +0100

    tab to spaces

--- examples/object-stream.js
+++ examples/object-stream.js
@@ -11,19 +11,19 @@ a Transform stream.
 */
 
 var JSONStringifier = function (){
-	stream.Transform.call (this, { objectMode: true });
+  stream.Transform.call (this, { objectMode: true });
 };
 
 util.inherits (JSONStringifier, stream.Transform);
 
 JSONStringifier.prototype._transform = function (obj, encoding, cb){
-	this.push (JSON.stringify (obj));
-	cb ();
+  this.push (JSON.stringify (obj));
+  cb ();
 };
 
 //Try passing any data type except a Buffer or a string
 //The object mode is automatically set, so no options are needed
 streamifier.createReadStream ({ a: 1, b: 2 })
-		.pipe (new JSONStringifier ())
-		.pipe (process.stdout);
+    .pipe (new JSONStringifier ())
+    .pipe (process.stdout);
 //{"a":1,"b":2}
